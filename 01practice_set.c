50dca5322c93a70ba491ca645b8982c582190077
mjolnircoders <64115807+mjolnircoders@users.noreply.github.com>
2021-10-01 07:07:37 +0530

    Update 01practice_set.c

--- 01practice_set.c
+++ 01practice_set.c
@@ -4,9 +4,9 @@ int main()
 {
     int a = 0;
 
-    if (a = 11)
-        printf("I am 11\n");
+    if (a = 13)
+        printf("I am 13\n");
     else
-        printf("I am not 11\n");
+        printf("I am not 13\n");
     return 0;
 }
